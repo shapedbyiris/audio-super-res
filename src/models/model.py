4f0fcbb2c65358160fc0ce2fbe5b1c652540f562
Ariel Elkin <ariel@shapedbyiris.com>
2020-12-08 18:13:18 +0100

    fix

--- src/models/model.py
+++ src/models/model.py
@@ -35,9 +35,12 @@ class Model(object):
 			pass # we will instead load the graph from a checkpoint
 		else:
 			# create input vars
-			X = tf.compat.v1.placeholder(tf.float32, shape=(None, None, 1), name='X')
-			Y = tf.compat.v1.placeholder(tf.float32, shape=(None, None, 1), name='Y')
-			alpha = tf.compat.v1.placeholder(tf.float32, shape=(), name='alpha') # weight multiplier
+			# X = tf.compat.v1.placeholder(tf.float32, shape=(None, None, 1), name='X')
+			# Y = tf.compat.v1.placeholder(tf.float32, shape=(None, None, 1), name='Y')
+			# alpha = tf.compat.v1.placeholder(tf.float32, shape=(), name='alpha') # weight multiplier
+			X = tf.Variable(shape=(None, None, 1), dtype=tf.float32, name='X')
+			Y = tf.Variable(shape=(None, None, 1), dtype=tf.float32, name='Y')
+			alpha = tf.Variable(shape=(), dtype=tf.float32, name='alpha')
 			# save inputs
 			self.inputs = (X, Y, alpha)
 			tf.compat.v1.add_to_collection('inputs', X)
